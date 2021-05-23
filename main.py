b1e018dcd4df27cbd51982f76e099e0ed2937b92
Matheus <matheusalexandre.sena99@gmail.com>
2021-05-23 20:55:10 -0300

    chore: retornando aulas por RA

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import settings.mongo_client
+
+mongo_col = settings.mongo_client.mongo_col
+
+app = FastAPI()
+
+@app.get("/aulas/{ra}")
+def listar_aulas(ra: int):
+  aulas = []
+
+  for data in mongo_col.find({"ra": ra}):
+    aula = {
+      'cod turma':data['cod_turma'],
+      'turma': data['turma'],
+      'prof_pratica': data['prof_pratica'],
+      'prof_teoria': data['prof_teoria'],
+      'teoria': data['teoria'],
+      'tpi' : data['tpi'],
+      'turno' :data['turno']
+    }
+
+    aulas.append(aula)
+
+  return {"aulas": aulas}
