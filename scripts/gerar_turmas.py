72604afbaf2556166f2a865c79ef6b045ed5d05e
Matheus <matheusalexandre.sena99@gmail.com>
2021-05-23 20:30:15 -0300

    refactor: separando scripts

--- scripts/gerar_turmas.py
+++ scripts/gerar_turmas.py
@@ -4,7 +4,7 @@ import settings.mongo_client
 
 mongo_col = settings.mongo_client.mongo_col
 
-df = pd.read_excel('./assets/turmas_e_docentes_2021_02.xlsx', header=None)
+df = pd.read_excel('../assets/turmas_e_docentes_2021_02.xlsx', header=None)
 
 df.columns = ['CURSO', 'CÓDIGO DE TURMA', 'TURMA', 'TEORIA', 'PRÁTICA', 'Campus', 'TURNO', 'T-P-I', 'DOCENTE TEORIA', 'DOCENTE PRÁTICA']
 
@@ -16,12 +16,12 @@ def insert_into_db(df1):
     cod_turma = str(row['CÓDIGO DE TURMA']).replace('\xad', '-')
 
     data = {
-      'teoria': str(row['TEORIA']).replace('\xad', '-'),
-      'pratica': str(row['PRÁTICA']).replace('\xad', '-'),
-      'turno': str(row['TURNO']).replace('\xad', '-'),
-      'tpi': str(row['T-P-I']).replace('\xad', '-'),
-      'prof_teoria': str(row['DOCENTE TEORIA']).replace('\xad', '-'),
-      'prof_pratica': str(row['DOCENTE PRÁTICA']).replace('\xad', '-')
+      'teoria': str(row['TEORIA']).replace('\xad', '-').replace('nan', ''),
+      'pratica': str(row['PRÁTICA']).replace('\xad', '-').replace('nan', ''),
+      'turno': str(row['TURNO']).replace('\xad', '-').replace('nan', ''),
+      'tpi': str(row['T-P-I']).replace('\xad', '-').replace('nan', ''),
+      'prof_teoria': str(row['DOCENTE TEORIA']).replace('\xad', '-').replace('nan', ''),
+      'prof_pratica': str(row['DOCENTE PRÁTICA']).replace('\xad', '-').replace('nan', '')
     }
     x = mongo_col.update_many({"cod_turma": cod_turma}, {"$set": data}, upsert=True, array_filters=None)
     print('atualizando:', cod_turma, index)
